c22547c9d710989350071f65f8d3c6e97362dca6
9crk <admin@9crk.com>
2016-01-27 21:33:19 +0800

    Update firstRun.sh

--- firstRun.sh
+++ firstRun.sh
@@ -3,3 +3,8 @@ cp RT2870STA.dat /etc/Wireless/RT2870STA/
 mkdir /usr/share/udhcpc/ -p
 mkdir /etc/udhcpc/ -p
 ln -s /hicat/scp /bin/scp 
+
+mkdir /var/lib/misc/ -p
+touch /var/lib/misc/udhcpd.leases
+ifconfig ra0 192.168.1.1
+udhcpd ./udhcpd.conf &
