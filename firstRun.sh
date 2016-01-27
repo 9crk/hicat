8ba425c0c65e702c300659d61483afbd102579de
zhouhua <admin@9crk.com>
2016-01-27 18:08:36 +0800

    hello

---
+++ firstRun.sh
@@ -0,0 +1,5 @@
+mkdir /etc/Wireless/RT2870STA/ -p
+cp RT2870STA.dat /etc/Wireless/RT2870STA/
+mkdir /usr/share/udhcpc/ -p
+mkdir /etc/udhcpc/ -p
+ln -s /hicat/scp /bin/scp 
--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+export PATH=$PATH:/hicat
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/hicat
 insmod ./mt7601Usta.ko
 ifconfig ra0 up
 ./wpa_supplicant -Dwext -i ra0 -c wpa_supplicant.conf -d &
